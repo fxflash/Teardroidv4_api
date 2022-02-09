4235f3bcb53df5ac5eb28f4bb1457b0215ecacdf
fxflash <99328055+fxflash@users.noreply.github.com>
2022-02-09 19:20:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from routers.auth import auth
 
 app = FastAPI(
     version="4.0",
-    title="Teardroid v2 - BOTNET",
-    description="Teardroid v2 - BOTNET",
+    title="fxdroid v2 - BOTNET",
+    description="fxdroid v2 - BOTNET",
     redoc_url=None,
 )
 
